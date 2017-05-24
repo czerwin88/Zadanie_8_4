99c2dc301dc890983a6b1beecde3d5df5a25c549
Czerwin <r.j.czerwinski@gmail.com>
2017-05-24 11:36:49 +0200

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
+//Var promt example
+
 console.log(variable name);
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
+
+// Var result
+
 console.log('Hello, ' + name);
